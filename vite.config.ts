8ec6a5ef3bc258e2208f139dfeb305d701d37c38
TAndrii789 <andrytomkiv789@gmail.com>
2024-09-25 20:38:55 +0200

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { createHtmlPlugin } from 'vite-plugin-html'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
-    // createHtmlPlugin(), 
+    createHtmlPlugin(), 
   ],
   base: '/Portfolio/',
-  // publicDir: 'public',
-  // resolve: {
-  //   extensions: ['.js', '.jsx', '.ts', '.tsx'], // Ensure Vite resolves both JS and TS extensions
-  // },
+  publicDir: 'public',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'], // Ensure Vite resolves both JS and TS extensions
+  },
 })
