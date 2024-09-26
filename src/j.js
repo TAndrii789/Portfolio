0d2881127a20aa212f70530f9be3faccc1c5df61
TAndrii789 <andrytomkiv789@gmail.com>
2024-09-26 14:40:30 +0200

    first commit

---
+++ src/j.js
@@ -0,0 +1,11 @@
+
+
+// setTimeout(() => {
+//   const e = document.querySelector('.spin-animation')
+// console.log(e);
+
+// e.addEventListener("animationend", ()=>{
+//   console.log("spin-animation ended!");
+
+// });
+// },500);
